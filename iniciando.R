f7f3e0c667667fc78050fb6307424ecc84a531fe
carlosafs <carlinhosfreitasee@gmail.com>
2019-05-09 12:13:49 -0300

    criando novo gráfico/visualização

--- iniciando.R
+++ iniciando.R
@@ -10,3 +10,12 @@ ggplot(data = mpg) +
             y = "Eficiência km/galão", 
             color = "Tipo de carro", 
             title = "Analise do tamanho do motor pelo consumo")
+
+mpg
+
+ggplot(data = mpg) + 
+  geom_violin(mapping = aes(x = class, y = hwy), color = "green") +
+    labs( x = "Tipo de carro", 
+      y = "Eficiência km/galão", 
+      title = "Analise do tipo de carro pelo consumo")
+    
